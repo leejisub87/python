85d6887c17246b24bf6c7be45ce27b5254fa480e
leejisub87 <63348385+leejisub87@users.noreply.github.com>
2021-04-25 18:11:54 +0900

    v 2.0

--- buying_module.py
+++ buying_module.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import pyupbit
 import time
+pd.set_option('display.max_columns', 20)
 ###########
 # input
 ############
@@ -50,14 +51,19 @@ def criteria_updown(df):
     avg_low = np.mean(df.low[0:len(df)-1])
     avg_close = np.mean(df.close[0:len(df)-1])
     avg_volume = np.mean(df.volume[0:len(df)-1])
-    open_rate = df.open[len(df)-1]/df.open[len(df)-2]
+    if len(df) > 1:
+        open_rate = df.open[1]/df.open[0]
+        volume_rate = df.volume[1] / df.volume[0]
+    else:
+        open_rate = 0
+        volume_rate = 0
     if open_rate < 1:
         open_status = 'down'
     elif open_rate > 1:
         open_status = 'up'
     else:
         open_status = 'normal'
-    volume_rate = df.volume[len(df)-1]/df.volume[len(df)-2]
+
     if volume_rate < 1 :
         volume_status = 'down'
     elif volume_rate > 1:
@@ -98,26 +104,22 @@ def criteria_boxplot(df):
 def check_buy_case(box):
     idx = len(box)
     result = False
-    if bool(np.sum(box.type == 'blue')>=3) & bool(box.chart_name[idx-1] in ['stone_cross','lower_tail_pole_umbong']):
-        result = True
-    elif bool(np.sum(box.type == 'blue')>=3) & bool(box.chart_name[idx-1] in ['dragonfly_cross', 'upper_tail_pole_yangong']):
+    if bool(np.sum(box.type == 'blue')>=4) & bool(box.chart_name[idx-1] in ['stone_cross', 'lower_tail_pole_umbong', 'dragonfly_cross', 'upper_tail_pole_yangong']):
         result = True
     elif bool(box.chart_name[idx-1] in ['pole_yangbong','longbody_yangbong']):
         result = True
-    elif bool(np.sum(box.type == 'blue')>=3) & bool(box.chart_name[idx-1] == 'pole_umbong'):
-        result = True
-    elif bool(np.sum(box.type == 'blue')>=4) & bool(box.chart_name[idx-1] in ['doji_cross','spinning_tops']):
+    elif bool(np.sum(box.type == 'blue')>=4) & bool(box.chart_name[idx-1] in ['pole_umbong', 'doji_cross','spinning_tops']):
         result = True
     return result
 
 def check_sell_case(box):
     idx = len(box)
     result = False
-    if bool(np.sum(box.type == 'red')>=3) & bool(box.chart_name[idx-1] in ['stone_cross','lower_tail_pole_umbong','upper_tail_pole_umbong']):
+    if bool(np.sum(box.type == 'red')>=4) & bool(box.chart_name[idx-1] in ['stone_cross', 'lower_tail_pole_umbong', 'upper_tail_pole_umbong']):
         result = True
-    elif bool(np.sum(box.type == 'red')>=4) & bool(box.chart_name[idx-2] in ['longbody_yangbong','shortbody_yangbong']):
+    elif bool(np.sum(box.type == 'red')>=4) & bool(box.chart_name[idx-1] in ['longbody_yangbong', 'shortbody_yangbong']):
         result = True
-    elif bool(np.sum(box.type == 'red')>=4) & bool(box.chart_name[idx-1] in ['spinning_tops','doji_cross']):
+    elif bool(np.sum(box.type == 'red')>=4) & bool(box.chart_name[idx-1] in ['spinning_tops', 'doji_cross']):
         result = True
     elif bool(box.volume_status[idx - 2] == 'up') & bool(box.open_status[idx - 2] == 'up') & bool(box.volume_status[idx - 1] == 'down') & bool(box.open_status[idx - 2] == 'down'):
         result = True
@@ -126,12 +128,14 @@ def check_sell_case(box):
 def coin_check(box):
     idx = len(box)
     volume_levels = np.sum(box.volume_rate > 1) # 관심도가 증가 구매/판매 우선순위
-    updown_levels = np.sum(box.open_rate > 1)   # 상승 강도 0~5 price 조정
+    updown_levels = np.sum(box.open_rate > 1)   # 상승 강도 0~4 price 조정
     avg_volume_rate = np.mean(box.volume_rate)
     avg_open_rate = np.mean(box.open_rate)
     check_buy = check_buy_case(box)
     check_sell = check_sell_case(box)
-    result = {'volume_levels': volume_levels,'avg_volume_rate':avg_volume_rate,'avg_open_rate': avg_open_rate, 'updown_levels': updown_levels, 'check_buy': check_buy, 'check_sell': check_sell}
+    buy_price = np.mean(box.avg_low) + 1.95 * np.std(box.avg_low)/5
+    sell_price = np.mean(box.avg_high) - 1.95 * np.std(box.avg_high)/5
+    result = {'diff_middle': buy_price, 'sell_price': sell_price,'buy_price': buy_price, 'volume_levels': volume_levels, 'avg_volume_rate': avg_volume_rate, 'avg_open_rate': avg_open_rate, 'updown_levels': updown_levels, 'check_buy': check_buy, 'check_sell': check_sell}
     return result
 
 def coin_info(coin_name, interval):
@@ -143,13 +147,20 @@ def coin_info(coin_name, interval):
     return result
 
 #호가를 이용
-def get_hoga_price(coin_name, no):
+def get_hoga_price(coin_name, price, type):
     # input : coint_name
     orderbook = pyupbit.get_orderbook(coin_name)
     df_orderbook = pd.DataFrame(orderbook[0]['orderbook_units'])
-    ask_price = df_orderbook.ask_price[no]
-    bid_price = df_orderbook.bid_price[no]
-    result = {'ask_price': ask_price, 'bid_price': bid_price}
+    if type == 'ask':
+        bid_price = df_orderbook.bid_price
+        idx = np.where(bid_price < price)
+        idx2 = list(idx[0])
+        result = list(bid_price[idx2])
+    if type == 'bid':
+        ask_price = df_orderbook.ask_price
+        idx = np.where(ask_price > price)
+        idx2 = list(idx[0])
+        result = list(ask_price[idx2])
     return result
 
 # 현재 내 코인 정보
@@ -166,51 +177,80 @@ def my_coin(upbit, remove_tickers):
         my_balances = my_balances[pd.to_numeric(my_balances.avg_buy_price) > 0]
     return my_balances
 
-def ask_coin(upbit, coin_name, investment, no):
+def ask_coin(upbit, coin_name, ask_price, investment):
     money = float(pd.DataFrame(upbit.get_balances())['balance'][0])
-    hoga_price = get_hoga_price(coin_name, no)
+    hoga_price = get_hoga_price(coin_name, ask_price, "ask")
+    current_price = pyupbit.get_current_price(coin_name)
     last_investment = min(investment, money)
-    price = hoga_price.get('ask_price')
-    count = last_investment / price
-    try:
-        result = upbit.buy_limit_order(coin_name, price, count)
-        print('상승 예측 / 매수 요청: ' + coin_name)
-    except:
-        print('매수 error: ' + coin_name)
-    return result
+    ask_list = []
+    for price in hoga_price:
 
-def bid_coin(upbit, coin_name, my_balances, cutoff, benefit, no):
+        count = last_investment / price
+        try:
+            result = upbit.buy_limit_order(coin_name, min(price, current_price), count)
+            time.sleep(0.01)
+            print('상승 예측 / 매수 요청: ' + coin_name)
+            if len(result)>2:
+                uuid = result.get('uuid')
+                ask_list.append(uuid)
+            else:
+                print("service error")
+        except:
+            print('매수 error: ' + coin_name)
+    return ask_list
+
+def bid_coin(upbit, coin_name, bid_price, my_balances, cutoff, benefit):
     df = my_balances[my_balances.coin_name == coin_name]
     df.reset_index(drop=True, inplace=True)
     avg_price = pd.to_numeric(df.avg_buy_price)[0]
-    hoga_price = get_hoga_price(coin_name, no)
-    price = hoga_price.get('bid_price')
+    hoga_price = get_hoga_price(coin_name, bid_price, "bid")
+    if len(hoga_price)>=3:
+        hoga_price = hoga_price[0:3]
     current_price = pyupbit.get_current_price(coin_name)
     ratio = (current_price - avg_price) / avg_price
     balance = float(df.balance[0])
-    result = []
-    try:
-        if ratio < (-cutoff): # 손절
+    if not hoga_price:
+        balance_n = balance
+        hoga_price = avg_price
+    else:
+        balance_n = balance / len(hoga_price)
+    bid_list = []
+    if ratio < (-cutoff): # 손절
+        try:
+            result = upbit.sell_limit_order(coin_name, current_price, balance)
             print("코인 손절 요청: "+coin_name)
-            result = upbit.sell_limit_order(coin_name, min(price, current_price), balance)
-        elif ratio > benefit:
-            print("코인 익절 요청: "+coin_name)
-            result = upbit.sell_limit_order(coin_name, max(price, current_price), balance)
+            if len(result) > 2:
+                uuid = result.get('uuid')
+                bid_list.append(uuid)
+            else:
+                print("service error")
+        except:
+            print("service error : 손절 시도")
+    elif ratio > benefit:
+        for price in hoga_price:
+            try:
+                result = upbit.sell_limit_order(coin_name, max(current_price, price), balance_n)
+                print("코인 익절 요청: "+coin_name)
+                if len(result) > 2:
+                    uuid = result.get('uuid')
+                    bid_list.append(uuid)
+                else:
+                    print("service error")
+            except:
+                print("service error : 익절 시도")
+    else:
+        cc = np.where(hoga_price > avg_price * 1.2)[0]
+        if len(cc) > 0:
+            no = max(cc)
+            result = upbit.sell_limit_order(coin_name, hoga_price[no], balance)
+            if len(result) > 2:
+                uuid = result.get('uuid')
+                bid_list.append(uuid)
+            else:
+                print("service error")
         else:
-            result = {'etc': 'wait coin'}
-    except:
-        print('매도 error: ' + coin_name)
-    return result
-
-def bid_coin_set(upbit, coin_name, remove_tickers, cutoff, benefit, no):
-    my_balances = my_coin(upbit, remove_tickers)
-    a = np.sum(my_balances.coin_name == coin_name)
-    money = pd.to_numeric(upbit.get_balances()[0].get('balance'))
-    result =[]
-    if bool(a > 1): # 탐색중 판매 신호가 발생
-        print("하락 예측 / 매도 요청: "+ coin_name)
-        result = bid_coin(upbit, coin_name, my_balances, cutoff, benefit)
-    return result
+            print("대기")
+    return bid_list
 
 def check_my_price(upbit, remove_tickers):
     my_balances = my_coin(upbit, remove_tickers)
@@ -237,101 +277,110 @@ def check_my_price(upbit, remove_tickers):
     result = {'my_money_value': round(my_money_value,2),'KRW': round(KRW,0), 'my_revenue': round(revenue,0), 'my_revenue_ratio': round(revenue_ratio,2), 'tot_price': round(a,0), 'tot_currency_price': round(b,0)}
     return result
 
-def main(upbit, interval, remove_tickers, investment, cutoff, benefit, ask_no,bid_no):
+def get_coin_selection(upbit):
+    start_time = time.time()
+    tickers = pyupbit.get_tickers(fiat="KRW")
+    my_balances = []
+    for coin_name in tickers:
+        res = coin_info(coin_name, interval="minute10")
+        my_balances.append(res)
+    df = pd.DataFrame(my_balances).sort_values('avg_volume_rate', ascending = False)
+    df = df[df.avg_volume_rate > 1]
+    df = df[df.updown_levels >= 1]
+    df = df[df.volume_levels >= 1]
+    result = df
+    end_time = time.time()
+    diff_time = end_time - start_time
+    print("후보 산출시간: "+str(diff_time)+"초")
+    return result
+
+def main(access_key, secret_key, interval, remove_tickers, investment, cutoff, benefit):
     print("시작")
+    upbit = pyupbit.Upbit(access_key, secret_key)
     validation = list()
     start_my_price = check_my_price(upbit, remove_tickers)
     start_time = time.time()
     start_my_price['time'] = 0
     validation.append(start_my_price)
-
+    favorite_coin = []
+    n = 0
+    m = 0
     while True:
-        tickers = pyupbit.get_tickers(fiat="KRW")
+        end_time = time.time()
+        diff_time = end_time - start_time
+        k = diff_time // 60*5
+        while k >= n:
+            n = n + 1
+            while len(favorite_coin) == 0:
+                favorite_coin = get_coin_selection(upbit)
+        tickers = list(favorite_coin.coin_name)
         tickers = [w for w in tickers if np.sum([str('KRW-'+c) in w for c in remove_tickers]) == 0]
+        start_time = time.time()
+        diff_time = 0
         result = []
-        buy_list = []
-        sell_list = []
-        # 구매/ 판매 시도
+        ask_list = []
+        bid_list = []
+
+        # 구매
         for coin_name in tickers:
-            coin_name = tickers[0]
             try:
                 money = float(pd.DataFrame(upbit.get_balances())['balance'][0])
-                my_balances = my_coin(upbit, remove_tickers)
-                sell_tickers = list(my_balances.coin_name)
                 res = coin_info(coin_name, interval)
                 result.append(res)
             except:
                 print("coin_info")
 
-            a = bool(res.get('avg_volume_rate')>1) & bool(res.get('avg_open_rate') > 1)
-            b = bool(coin_name in sell_tickers)
+            a = bool(res.get('avg_volume_rate') > 1) & bool(res.get('avg_open_rate') > 1)
             if bool(a | res.get('check_buy')) & bool(money > 5000):
                 try:
-                    ask_result = ask_coin(upbit, coin_name, investment, ask_no)
-                    if len(list(ask_result)) > 2:
-                        print("매수 요청: " + coin_name)
-                        buy_list.append(ask_result)
-                        time.sleep(0.1)
+                    ask_price = res.get('buy_price')
+                    ask_result = ask_coin(upbit, coin_name, ask_price, investment)
+                    if not ask_result:
+                        print("null")
                     else:
-                        print(ask_result.get('error'))
+                        ask_list.append(ask_result)
                 except:
                     print("매수 error :" + coin_name)
-            elif a | res.get('check_sell'):
-                try:
-                    bid_result = bid_coin_set(upbit, coin_name, remove_tickers, cutoff, benefit, bid_no)
-                    if len(list(bid_result)) > 2:
-                        sell_list.append(bid_result)
-                        time.sleep(0.1)
-                except:
-                    print("매도(하락) error :"+coin_name)
-            elif b:
-                try:
-                    bid_result = bid_coin(upbit, coin_name, my_balances, cutoff, benefit, bid_no)
-                    if len(list(bid_result)) > 2:
-                        sell_list.append(bid_result)
-                        time.sleep(0.1)
-                except:
-                    print('매도 error: ' + coin_name)
 
+        # 판매
+        my_balances = my_coin(upbit, remove_tickers)
+        sell_tickers = list(my_balances.coin_name)
+        for coin_name in sell_tickers:
+            try:
+                res = coin_info(coin_name, interval)
+                bid_price = res.get('sell_price')
+                bid_result = bid_coin(upbit, coin_name, bid_price, my_balances, cutoff, benefit)
+                if not bid_result:
+                    print("null")
+                else:
+                    bid_list.append(bid_result)
+            except:
+                print('매도 error: ' + coin_name)
+
+        end_time = time.time()
+        diff_time = end_time - start_time
+
+        time.sleep(5)
+        diff_time = end_time - start_time
+        k = diff_time // 60 * 5
+        while k >= m:
+            m = m + 1
         # 예약 취소
-        buying_reservation_coin_cancel(upbit, sell_list)
-        buying_reservation_coin_cancel(upbit, buy_list)
+            buying_reservation_coin_cancel(upbit, ask_list)
+            buying_reservation_coin_cancel(upbit, bid_list)
         end_my_price = check_my_price(upbit, remove_tickers)
-
         end_time = time.time()
         diff_time = round((end_time - start_time),1)
         end_my_price['run_time'] = diff_time
-        print('수익 :'+str(end_my_price.get('KRW')+end_my_price.get('my_revenue'))+"("+str(end_my_price.get('my_revenue_ratio')))
+        print('수익 :'+str(end_my_price.get('KRW')+end_my_price.get('my_revenue'))+"("+str(end_my_price.get('my_revenue_ratio'))+")")
         validation.append(end_my_price)
     return result
 
-#접속
-def connection_upbit(case):
-    #case = 'work'
-    if case == 'home':
-        access_key = 'DXqzxyCQkqL9DHzQEyt8pK5izZXU03Dy2QX2jAhV'
-        secret_key = 'x6ubxLyUVw03W3Lx5bdvAxBGWI7MOMJjblYyjFNo'
-    elif case == 'work':
-        access_key = 'Ae8p07023M16i2b0ONBRs0YVf2yBUcvvBpKjZrT5'
-        secret_key = 'U1Fo9YcEzq9SR6T31JbXsvyOWREy4kBYs5PGx2Hs'
-    upbit = pyupbit.Upbit(access_key, secret_key)
-    return upbit
+
 
 #매수 전체 취소 프로세스
-def buying_reservation_coin_cancel(upbit, variable_list):
-    if len(variable_list)==1:
-        uuids = variable_list[0].get('uuid')
-        try:
-            for uuid in uuids:
-                upbit.cancel_order(uuid)
-                time.sleep(0.1)
-        except:
-            print("예약된 구매 리스트가 없습니다.")
-    else:
-        try:
-            uuids = list(pd.DataFrame(variable_list)['uuid'].dropna())
-            for uuid in uuids:
-                upbit.cancel_order(uuid)
-                time.sleep(0.1)
-        except:
-            print( "예약된 구매 리스트가 없습니다.")
+def buying_reservation_coin_cancel(upbit, v_list):
+    for i in range(len(v_list)):
+        for j in range(len(v_list[i])):
+            upbit.cancel_order(v_list[i][j])
+            time.sleep(0.1)
--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import buying_module as co
-# pd.set_option('display.max_columns', 20)
 
 #접속
 intervals = ["minute1", "minute3", "minute5", "minute10", "month", "days", "minute240", "minute60", "minute30", "minute15"]
-upbit = co.connection_upbit('home')
 
 ##################################
 # input
 ##################################
-interval = "minute1"
+access_key = '0PUcnCxxr9pax8xExCpLvMndcIpMcY38P9Q3cfRH'
+secret_key = 'xxACrqJLnXj0JH9DLP0839RWnpN2Izrne6mZZoMx'
+interval = "minute3"
 remove_tickers = ['KRW', 'XYM']
 investment = 10000
 cutoff = 0.01  # 1% 손절
 benefit = 0.02  # 1% 익절
-ask_no = 1
-bid_no = 1
 
-result = co.main(upbit, interval, remove_tickers, investment, cutoff, benefit, ask_no, bid_no)
+result = co.main(access_key, secret_key, interval, remove_tickers, investment, cutoff, benefit)
