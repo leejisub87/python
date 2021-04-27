b176ba539c234fdf3e932d4e453bcf675c28f53f
leejisub87 <63348385+leejisub87@users.noreply.github.com>
2021-04-28 07:51:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ def reservation_cancel(upbit):
 def execute_sell_schedule(upbit, sell_df, cutoff, get_benefit):
     # mdf = sell_df
     # sell_df = new_df
-
     benefit_min = get_benefit.get('benefit_min')
     benefit_max = get_benefit.get('benefit_max')
     my_coin = pd.DataFrame(upbit.get_balances())
@@ -113,69 +112,70 @@ def execute_sell_schedule(upbit, sell_df, cutoff, get_benefit):
     return result
 
     # 판매 가격 결정하기...
-def execute_buy_schedule(upbit, mdf, investment):
+
+
+def excute_buy_df(df, coin_name,investment):
+    status_list = []
+    for i in range(len(df)):
+        #i =0
+        df = df[i:i + 1]
+        status = 'wait'
+        max_price = df.buy_price_max - 1.96 * np.sqrt(np.std(df.buy_price_max) / len(df.buy_price_max))
+        updown_levels = np.mean(df.influence_m)
+        volume_levels = np.mean(df.influence_v)
+        levels = updown_levels + volume_levels
+        weight = 1 + levels / 8
+        # 구매 가격 결정하기
+        money = float(pd.DataFrame(upbit.get_balances())['balance'][0])
+        if money < 5000:
+            pass
+        else:
+            last_investment = max(investment, money)
+            hoga = get_hoga_price(coin_name)
+            default = max_price[0]
+            criteria = max(hoga.get('ask_price')) > default > min(hoga.get('bid_price'))
+            if not criteria:
+                print(coin_name + "은 구매 계획에 도달하지 않았다.")
+            else:
+                print(coin_name + "은 구매 계획에 도달했다.")
+                ho = list(hoga.get('bid_price')[hoga.get('bid_price') <= default])
+                if len(ho) >= 3:
+                    price_set = ho[-3:]
+                else:
+                    price_set = ho
+
+                for price in price_set:
+                    #price = price_set[0]
+                    count = investment / price
+                    try:
+                        res = upbit.buy_limit_order(coin_name, price, count)
+                        time.sleep(0.1)
+                        if len(res) > 2:
+                            print(coin_name + "을 " + str(price) + "에" + " 구매요청합니다.")
+                            save_uuid(res)
+                            load_uuid()
+                            status = 'res_ask'
+                            status_list.append(status)
+                    except:
+                        print('매수 error: ' + coin_name)
+    return status_list
+def execute_buy_schedule(upbit, buy_df, investment):
     money = float(pd.DataFrame(upbit.get_balances())['balance'][0])
     if money < 5000:
         print("주문금액 부족")
-        result = mdf
+        result = buy_df
     else:
         # mdf = buy_df
-        mdf.reset_index(drop=True, inplace=True)
-        status_list = []
-        tickers = list(set(mdf.coin_name)) # 구매신청
+        buy_df.reset_index(drop=True, inplace=True)
+        first_res = []
+        tickers = list(set(buy_df.coin_name)) # 구매신청
+        n = 0
         for coin_name in tickers:
-            #coin_name ='KRW-XRP'
-            df_buy = mdf[mdf.coin_name == coin_name]
-            status = 'wait'
-            currency_time = datetime.now()
-            start_date_ask = np.min(df_buy.start_date_ask)
-            end_date_ask = np.max(df_buy.end_date_ask)
-            min_price = np.mean(df_buy.buy_price_max) - 1.96 * np.sqrt(np.std(df_buy.buy_price_max)/len(df_buy.buy_price_max))
-            max_price = np.mean(df_buy.sell_price_min) - 1.96 * np.sqrt(np.std(df_buy.sell_price_min)/len(df_buy.sell_price_min))
-            updown_levels = np.mean(df_buy.influence_m)
-            volume_levels = np.mean(df_buy.influence_v)
-            levels = updown_levels + volume_levels
-            weight = 1 + levels/8
-            # 구매 가격 결정하기
-            try:
-                money = float(pd.DataFrame(upbit.get_balances())['balance'][0])
-                time.sleep(0.1)
-            except:
-                money = investment
-
-            if money < 5000:
-                pass
-            else:
-                last_investment = min(investment, money)
-                hoga = get_hoga_price(coin_name)
-                default = min_price
-                if bool(min(hoga.get('bid_price')) > min_price):
-                    print(coin_name+"은 구매 계획에 도달하지 않았다.")
-                else:
-                    print(coin_name+ "은 구매 계획에 도달했다.")
-                    ho = list(hoga.get('bid_price')[hoga.get('bid_price') <= min_price])
-                    if len(ho)>=3:
-                        price_set = ho[-3:]
-                    else:
-                        price_set = ho
-
-                    for price in price_set:
-                        count = investment / price
-                        try:
-                            res = upbit.buy_limit_order(coin_name, price, count)
-                            time.sleep(0.1)
-                            if len(res) > 2:
-                                print(coin_name +"을 "+str(price)+"에" + " 구매요청합니다.")
-                                save_uuid(res)
-                                status = 'res_ask'
-                                status_list.append(status)
-                        except:
-                            print('매수 error: ' + coin_name)
-        df_buy.reset_index(drop=True, inplace=True)
-        status_df = pd.DataFrame(status_list, columns=['status'], index=[0])
-        result = pd.concat([status_df, df_buy], axis=1)
+            #coin_name = tickers[1]
+            df = buy_df[buy_df.coin_name == coin_name]
+            df.reset_index(drop=True, inplace=True)
+            status = excute_buy_df(df, coin_name,investment)
     return result
-
 def check_buy_case(df):
     idx = len(df)
     result = False
@@ -416,27 +416,25 @@ def coin_validation(upbit, tickers, interval):
     diff = round(et-st,2)
     print("분석 종료 : " + str(diff)+"초")
     return result
-def save_uuid(dict):
+def save_uuid(uuid):
+    #uuid = res
     # uuid -> dataframe, 기존의 uuid를 불러온다. uuid를 저장한다
-    df = pd.DataFrame(dict, index=[0])
-    df = df.dropna(axis=0)
+    df = pd.DataFrame([uuid.get('uuid')], columns=['uuid'])
+    #df = df.dropna(axis=0)
 
-    dt_now = datetime.now()
-    now = dt_now.strftime('%Y%m%d')
-    name = 'reservation_'+now+'.json'
+    name = 'reservation_list.json'
     directory = 'reservation'
     json_file = directory+'/'+name
     # 폴더생성
     if not os.path.exists(directory):
         os.makedirs(directory)
         # 데이터 읽고 머지
-    if os.path.isfile(directory):
+    if not os.path.isfile(directory):
         name = os.listdir(directory)[-1]
         json_file = directory + '/' + name
         ori_df = pd.read_json(json_file, orient='table')
-        ori_df = pd.DataFrame(ori_df)
-        df = pd.concat([ori_df, df], axis=0)
-
+        df = pd.concat([ori_df.uuid, ori_df.uuid], axis=0)
+    os.remove(json_file)
     # 저장
     df.to_json(json_file, orient='table')
 def load_uuid():
@@ -450,10 +448,12 @@ def load_uuid():
     else:
         print("예약 내역이 없습니다.")
     return ori_df
+def convert(set):
+    return [*set, ]
 def reservation_cancel(upbit):
     df = load_uuid()
     if len(df) > 0:
-        uuids = list(set(df.uuid))
+        uuids = convert(set(df.uuid))
         while len(uuids)>0:
             for uuid in uuids:
                 status = 'wait'
@@ -478,30 +478,25 @@ def load_df():
     ori_df = pd.DataFrame(ori_df)
     return ori_df
 def merge_df(df):
+    #df = new_df
     dt_now = datetime.now()
-    now = dt_now.strftime('%Y%m%d')
-    name = 'schedule_' + now + '.json'
+    name = 'schedule_list.json'
     directory = 'schedule'
     json_file = directory + '/' + name
 # 폴더생성
     if not os.path.exists(directory):
         os.makedirs(directory)
-
-    df = df.dropna(axis=0)
     df = pd.DataFrame(df)
-    if os.path.isfile(directory):
+    if not os.path.isfile(directory):
         name = os.listdir(directory)[-1]
         json_file = directory + '/' + name
         ori_df = pd.read_json(json_file, orient='table')
-        ori_df = pd.DataFrame(ori_df, axis=0)
-        df = pd.concat([ori_df, df], axis=0)
-    else:
-        df = df
+        df = pd.concat([ori_df.reset_index(drop=True), df.reset_index(drop=True)], axis=0)
     #데이터 읽고 머지
+    df = df[df.start_date_bid < dt_now]
+    df = df[dt_now < df.end_date_bid]
     df.to_json(json_file, orient='table')
-    #저장
-    result = df
-    return result
+    return df
 #구매 평가 요소. 주 동향,
 
 def generate_benefit_cutoff(new_df):
@@ -513,11 +508,10 @@ def generate_benefit_cutoff(new_df):
 
 def coin_trade(upbit, interval, total_updown, investment, cutoff):
     tickers = pyupbit.get_tickers(fiat="KRW")
+    intervals = ["month", "week", "day", "minute240", "minute60", "minute30", "minute10", 'minute5']
     print("현황 분석 시작.")
-    try:
-        new_df = load_df()
-    except:
-        new_df = coin_validation(upbit, tickers, interval)
+    #new_df = load_df()
+    new_df = coin_validation(upbit, tickers, interval)
         # 저장된 기존 df 를 불러오고 현재 생성된 df를 합친다.
     print("현황 분석 완료.")
 
@@ -526,8 +520,9 @@ def coin_trade(upbit, interval, total_updown, investment, cutoff):
     benefit_max = get_benefit.get('benefit_max')
     result = get_benefit.get('up_count')
     print("총 코인 수: "+str(len(new_df))+"개, 현재 상승 코인수: "+str(result)+"개, 예상 수익률 : "+str(round(benefit_min,2)*100)+"% ~ "+str(round(benefit_max,2)*100)+"%")
+
+
     buy_df = new_df[new_df.check_buy]
-    buy_df.reset_index(drop=True, inplace=True)
     buy_df = execute_buy_schedule(upbit, buy_df, investment)
     sell_df = new_df
 
@@ -541,7 +536,6 @@ def coin_trade(upbit, interval, total_updown, investment, cutoff):
     print("schedual generate 종료(" + interval + "): " + str(round(diff, 1)) + '초')
 
     reservation_cancel(upbit)
-    coin_validation(upbit, tickers, interval)
 
     return result
 
